d8d96cb693932dd8eb2ea7931b0615c47f271f50
Wesley Friedman <friedman.wesley@gmail.com>
2016-08-26 00:46:08 +0000

    Done.

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,3 +1,19 @@
 def nyc_pigeon_organizer(data)
-  # write your code here!
+  pigeon_list = {}
+  data.each do |info, descriptor_hash|
+    descriptor_hash.each do |descriptors, names|
+      names.each do |name|
+        if pigeon_list.key?(name)
+          if pigeon_list[name].key?(info)
+            pigeon_list[name][info] << descriptors.to_s
+          else
+            pigeon_list[name][info] = [descriptors.to_s]
+          end
+        else
+          pigeon_list[name] = {info => [descriptors.to_s]}
+        end
+      end
+    end
+  end
+  return pigeon_list
 end
